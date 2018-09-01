1938ac514d989aef92d9c208f04fd5bbc0921934
notisrac <notisrac@gmail.com>
2018-09-01 16:16:13 +0200

    Added the loadingIndicator
    Added hublist and connections component to the server page
    Added sidebarController

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -6,17 +6,6 @@ var softEtherAdminApp = angular.module('softEtherAdminApp', [
     'serverModule'
 ]);
 
-softEtherAdminApp.controller('HeaderController', function ($scope, $http) {
-    var self = this;
-    self.hostName = '-unknown-';
-    
-    $http.get('api/server/info').then(function(response) {
-        var data = response.data;
-        self.hostName = data['Host Name'];
-        self.version = data['Version'];
-    });
-});
-
 softEtherAdminApp.config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
@@ -24,3 +13,71 @@ softEtherAdminApp.config(['$locationProvider', '$routeProvider',
         $routeProvider.otherwise({redirectTo: '/dashboard'});
     }
 ]);
+
+softEtherAdminApp.controller('HeaderController', function ($scope, $http) {
+    $scope.loading = true;
+    
+    $http.get('api/server/info').then(function(response) {
+        var data = response.data;
+        $scope.hostName = data['Host Name'];
+        $scope.version = data['Version'];
+    }, function (reason) {
+        console.log(reason);        
+        $scope.hostName = '-unknown-';
+    }).finally(function () {
+        $scope.loading = false;
+    });
+});
+
+softEtherAdminApp.controller('SidebarController', function ($scope, $location) {
+    $scope.navData = [
+        {
+            isActive: false,
+            path: 'dashboard',
+            icon: 'pe-7s-graph',
+            text: 'Dashboard'
+        },
+        {
+            isActive: false,
+            path: 'server',
+            icon: 'pe-7s-server',
+            text: 'Server'
+        },
+        {
+            isActive: false,
+            path: 'hubs',
+            icon: 'pe-7s-share',
+            text: 'Hubs'
+        },
+        {
+            isActive: false,
+            path: 'config',
+            icon: 'pe-7s-config',
+            text: 'Config'
+        }
+    ];
+
+    $scope.$on('$locationChangeSuccess', function(event) {
+        var path = $location.path();
+        for (let i = 0; i < $scope.navData.length; i++) {
+            if (path.startsWith('/' + $scope.navData[i].path)) {
+                $scope.navData[i].isActive = true;
+            }
+            else {
+                $scope.navData[i].isActive = false;
+            }
+        }
+    });
+});
+
+softEtherAdminApp.component('loadingIndicator', {
+    templateUrl: 'loadingIndicator.html',
+    bindings: {
+        errorMessage: '<',
+        loading: '<'
+    },
+    controller: function () {
+        // var ctrl = this;
+        // console.log(ctrl.loading + ' - ' + ctrl.errorMessage);        
+    }
+});
--- public/js/app/server/server.js
+++ public/js/app/server/server.js
@@ -1,16 +1,53 @@
 'use strict';
 
-angular.module('serverModule', [
-    'ngRoute'
-])
+var module = angular.module('serverModule', [
+  'ngRoute'
+]);
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/server', {
-      templateUrl: 'js/app/server/server.template.html',
-      controller: 'server'
+module.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/server', {
+    templateUrl: 'js/app/server/server.template.html',
+    controller: 'ServerController'
+  });
+}])
+
+module.controller('ServerController', function ($scope, $http) {
+});
+
+module.component('hubList', {
+  templateUrl: 'js/app/server/server.hublist.template.html',
+  bindings: {},
+  controller: function ($http) {
+    var ctrl = this;
+    ctrl.loading = true;
+    ctrl.errorMessage = null;
+
+    $http.get('api/server/hublist').then(function (response) {
+      ctrl.data = response.data;
+    }, function (reason) {
+      console.log(reason);
+      ctrl.errorMessage = reason;
+    }).finally(function () {
+      ctrl.loading = false;
     });
-  }])
-  
-.controller('server', [function() {
-  // TODO fill me
-}]);
+  }
+});
+
+module.component('connectionsList', {
+  templateUrl: 'js/app/server/server.connections.template.html',
+  bindings: {},
+  controller: function ($http) {
+    var ctrl = this;
+    ctrl.loading = true;
+    ctrl.errorMessage = null;
+
+    $http.get('api/server/connectionList').then(function (response) {
+      ctrl.data = response.data;
+    }, function (reason) {
+      console.log(reason);
+      ctrl.errorMessage = reason;
+    }).finally(function () {
+      ctrl.loading = false;
+    });
+  }
+});
